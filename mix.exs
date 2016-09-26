057afd928adf60a42dab33646c6cb43faebc2ea8
helderhenri <helderhenri@gmail.com>
2016-09-26 15:04:06 -0300

    Add files via upload

---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule PhoenixSlime.Mixfile do
+  use Mix.Project
+
+  @version "0.7.0"
+
+  def project do
+    [
+      app: :phoenix_slime,
+      deps: deps,
+      description: """
+      Phoenix Template Engine for Slim-like templates
+      """,
+      elixir: "~> 1.0",
+      package: package,
+      version: @version]
+  end
+
+  def application do
+    [applications: [:phoenix, :slime]]
+  end
+
+  def deps do
+    [{:phoenix, "~> 1.1 or ~> 1.2"},
+     {:phoenix_html, "~> 2.3"},
+     {:cowboy, "~> 1.0"},
+     {:slime, "~> 0.14"}]
+  end
+
+  defp package do
+    [maintainers: ["Sean Callan"],
+     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/slime-lang/phoenix_slime"}]
+  end
+end
