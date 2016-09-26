ff526ec4d6ed3c47937e518527854c1e3a38a9ee
Helder de Sousa <helderhenri@gmail.com>
2016-09-26 16:47:33 -0300

    mix

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule PhoenixCalime.Mixfile do
     [{:phoenix, "~> 1.1 or ~> 1.2"},
      {:phoenix_html, "~> 2.3"},
      {:cowboy, "~> 1.0"},
-     {:calime,github: "helderhenri/calime" }]
+     {:calime, github: "helderhenri/calime", branch: "master"}]
   end
 
   defp package do
